f7d6baf39c9c593c47cf2ad28ade85b15ec4e239
Paul Kekejian <pkekejian@gmail.com>
2024-09-28 07:08:22 -0400

    I've added an Animal class and print function

--- PythonProject.py
+++ PythonProject.py
@@ -2,3 +2,14 @@ def git_push_confirm():
     print("I pushed by first file to Github!")
 
 git_push_confirm()
+
+# Made a small change below
+
+class Animals:
+    def __init__(self,name,diet):
+        self.animal_name = name
+        self.animal_diet = diet
+
+animal1 = Animals("Tiger","Meats")
+
+print(f"My favorite animal is a {animal1.animal_name} because they like to eat {animal1.animal_diet} just like me!")
